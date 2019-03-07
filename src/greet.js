f8e76256e89367ff7c360bb34d529f07abd67118
radeth <123radeth@gmail.com>
2019-03-08 00:19:33 +0100

    webpack.config.js added

---
+++ src/greet.js
@@ -0,0 +1,4 @@
+const greet = (name) =>{
+    return `hello ${name}`
+}
+export default greet
---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'main.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin()
+  ]
+}
