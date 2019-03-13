c051723ce11f7a7df6499fcdd9d86f87f688af46
radeth <123radeth@gmail.com>
2019-03-13 22:55:42 +0100

    vanila js and scss

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,3 @@
-import '../css/style.scss';
-import './../home.html'
-import { RandomGenerator } from './RadndomGenerator';
-
-const outputParagraph = document.querySelector('#outputParagraph');
-
-const outputRandomInt = () => {
-    outputParagraph.textContent = RandomGenerator.randomInteger();
-};
-
-const outputRandomRange = () => {
-    outputParagraph.textContent = RandomGenerator.randomRange(1, 500);
-};
-
-const buttonRndInt = document.querySelector('#randomInt');
-const buttonRndRange = document.querySelector('#randomRange');
-
-buttonRndInt.addEventListener('click', outputRandomInt);
-buttonRndRange.addEventListener('click', outputRandomRange);
+import '../css/style.scss'
 
 
