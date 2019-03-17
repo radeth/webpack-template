a0ae9277eef64ebfc057382697da187a1a1bb513
radeth <123radeth@gmail.com>
2019-03-17 21:38:39 +0100

    no jquery

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
         }
       },
       {
-        test: /\.scss$/,
+        test: /\.(css|scss)$/,
         use: [
             process.env.NODE_ENV === 'production' ? 'style-loader' : MiniCssExtractPlugin.loader,
             "css-loader",
